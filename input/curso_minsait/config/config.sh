a0b3a8f1e415181e13a70de225ccaa73a52f8614
marcelogsrodrigues gomes <mgsr02@gmail.com>
2023-06-05 17:16:39 +0000

    release 05/0/2023 01

--- input/curso_minsait/config/config.sh
+++ input/curso_minsait/config/config.sh
@@ -1,1 +1,8 @@
-# config
+#!/bin/bash
+
+DATE="$(date --date="-0 day" "+%Y%m%d")"
+
+TABLES=("cidade" "estado" "filial" "parceiro" "cliente" "subcategoria" "categoria" "item_pedido" "produto")
+TABELA_CLIENTE="TBL_CLIENTE_OLD"
+
+PARTICAO=""
--- input/curso_minsait/scripts/pre_process/download.sh
+++ input/curso_minsait/scripts/pre_process/download.sh
@@ -1,5 +1,32 @@
-# carga categoria
-cd ../../raw
-mkdir categoria
-cd categoria 
-curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+#!/bin/bash
+
+BASEDIR="$( cd "$( dirname "$(BASE_SOURCE[0]}" )" && pwd )"
+CONFIG="${BASEDIR}/../../config/config.sh"
+source "${CONFIG}"
+
+echo "Iniciando a criacao em ${DATE}"
+
+for table in "${TABLES[@]}"
+do
+    echo "tabela $table"
+    cd ../../raw
+    mkdir $table
+    chmod 777 $table
+    cd $table
+    curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$table.csv
+
+    #hdfs dfs -mkdir /tatalake/raw/$table
+    #hdfs dfs -chmod 777 /datalae/raw/$table
+    #hdfs dfs -copyFromLocal $table.csv /datalake/raw/$table
+    #beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$table.hql
+done
+
+echo "Finalizando a criacao em ${DATE}"
+
+
+
+# carga unitaria
+# cd ../../raw
+# mkdir categoria
+# cd categoria 
+# curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
